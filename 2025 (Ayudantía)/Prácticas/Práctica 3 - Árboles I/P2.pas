472521772dbf093174f4ef7b4f934991d2b63644
Federico Dobal <130794002+fededobal@users.noreply.github.com>
2025-08-30 16:19:19 -0300

    Create P2.pas

---
+++ 2025 (Ayudantía)/Prácticas/Práctica 3 - Árboles I/P2.pas
@@ -0,0 +1,239 @@
+program ProcesamientoVentas;
+
+type
+    venta = record
+        codigo: integer;
+        fecha: integer; 
+        cantidad: integer;
+    end;
+    
+    listaVentas = ^nodoLista;
+    nodoLista = record
+        dato: venta;
+        sig: listaVentas;
+    end;
+    
+    arbolVentas = ^nodoVentas;
+    nodoVentas = record
+        dato: venta;
+        HI: arbolVentas;
+        HD: arbolVentas;
+    end;
+
+    productoTotal = record
+        codigo: integer;
+        totalUnidades: integer;
+    end;
+    
+    arbolTotalizado = ^nodoTotalizado;
+    nodoTotalizado = record
+        dato: productoTotal;
+        HI: arbolTotalizado;
+        HD: arbolTotalizado;
+    end;
+    
+    productoConLista = record
+        codigo: integer;
+        ventas: listaVentas;
+    end;
+
+    arbolConLista = ^nodoConLista;
+    nodoConLista = record
+        dato: productoConLista;
+        HI: arbolConLista;
+        HD: arbolConLista;
+    end;
+
+procedure GenerarDatosYArboles(var aVentas: arbolVentas; var aTotal: arbolTotalizado; var aLista: arbolConLista);
+
+    procedure InsertarVenta(var a: arbolVentas; v: venta);
+    begin
+        if (a = nil) then
+        begin
+            new(a);
+            a^.dato := v;
+            a^.HI := nil;
+            a^.HD := nil;
+        end
+        else if (v.codigo < a^.dato.codigo) then
+            InsertarVenta(a^.HI, v)
+        else
+            InsertarVenta(a^.HD, v);
+    end;
+
+    procedure InsertarOActualizarTotal(var a: arbolTotalizado; v: venta);
+    begin
+        if (a = nil) then
+        begin
+            new(a);
+            a^.dato.codigo := v.codigo;
+            a^.dato.totalUnidades := v.cantidad;
+            a^.HI := nil;
+            a^.HD := nil;
+        end
+        else if (v.codigo = a^.dato.codigo) then
+            a^.dato.totalUnidades := a^.dato.totalUnidades + v.cantidad
+        else if (v.codigo < a^.dato.codigo) then
+            InsertarOActualizarTotal(a^.HI, v)
+        else
+            InsertarOActualizarTotal(a^.HD, v);
+    end;
+
+    procedure AgregarAdelante(var L: listaVentas; v: venta);
+    var nue: listaVentas;
+    begin
+        new(nue);
+        nue^.dato := v;
+        nue^.sig := L;
+        L := nue;
+    end;
+    
+    procedure InsertarOActualizarLista(var a: arbolConLista; v: venta);
+    begin
+        if (a = nil) then
+        begin
+            new(a);
+            a^.dato.codigo := v.codigo;
+            a^.dato.ventas := nil;
+            AgregarAdelante(a^.dato.ventas, v);
+            a^.HI := nil;
+            a^.HD := nil;
+        end
+        else if (v.codigo = a^.dato.codigo) then
+            AgregarAdelante(a^.dato.ventas, v)
+        else if (v.codigo < a^.dato.codigo) then
+            InsertarOActualizarLista(a^.HI, v)
+        else
+            InsertarOActualizarLista(a^.HD, v);
+    end;
+
+var
+    v: venta;
+begin
+    aVentas := nil;
+    aTotal := nil;
+    aLista := nil;
+    randomize;
+    v.codigo := 1 + random(20);
+    while (v.codigo <> 0) do
+    begin
+        v.fecha := 1 + random(31); 
+        v.cantidad := 1 + random(50);
+        writeln('Venta generada - Codigo: ', v.codigo, ' Fecha: ', v.fecha, ' Cant: ', v.cantidad);
+
+        InsertarVenta(aVentas, v);
+        InsertarOActualizarTotal(aTotal, v);
+        InsertarOActualizarLista(aLista, v);
+        
+        if (random(5) > 0) then 
+            v.codigo := 1 + random(20)
+        else
+            v.codigo := 0;
+    end;
+end;
+
+function TotalVendidoEnFecha(a: arbolVentas; f: integer): integer;
+var
+    cantActual: integer;
+begin
+    if (a = nil) then
+        TotalVendidoEnFecha := 0
+    else
+    begin
+        if (a^.dato.fecha = f) then
+            cantActual := a^.dato.cantidad
+        else
+            cantActual := 0;
+        TotalVendidoEnFecha := cantActual + TotalVendidoEnFecha(a^.HI, f) + TotalVendidoEnFecha(a^.HD, f);
+    end;
+end;
+
+function ProductoConMayorVentaTotal(a: arbolTotalizado): integer;
+    
+    procedure BuscarMax(a: arbolTotalizado; var maxCant: integer; var maxCod: integer);
+    begin
+        if (a <> nil) then
+        begin
+            if (a^.dato.totalUnidades > maxCant) then
+            begin
+                maxCant := a^.dato.totalUnidades;
+                maxCod := a^.dato.codigo;
+            end;
+            BuscarMax(a^.HI, maxCant, maxCod);
+            BuscarMax(a^.HD, maxCant, maxCod);
+        end;
+    end;
+
+var
+    maxCantidad, maxCodigo: integer;
+begin
+    maxCantidad := -1;
+    maxCodigo := -1;
+    BuscarMax(a, maxCantidad, maxCodigo);
+    ProductoConMayorVentaTotal := maxCodigo;
+end;
+
+function ProductoConMasCantidadDeVentas(a: arbolConLista): integer;
+
+    function ContarNodosLista(L: listaVentas): integer;
+    var cant: integer;
+    begin
+        cant := 0;
+        while (L <> nil) do
+        begin
+            cant := cant + 1;
+            L := L^.sig;
+        end;
+        ContarNodosLista := cant;
+    end;
+
+    procedure BuscarMaxVentas(a: arbolConLista; var maxCantVentas: integer; var maxCod: integer);
+    var cantActual: integer;
+    begin
+        if (a <> nil) then
+        begin
+            cantActual := ContarNodosLista(a^.dato.ventas);
+            if (cantActual > maxCantVentas) then
+            begin
+                maxCantVentas := cantActual;
+                maxCod := a^.dato.codigo;
+            end;
+            BuscarMaxVentas(a^.HI, maxCantVentas, maxCod);
+            BuscarMaxVentas(a^.HD, maxCantVentas, maxCod);
+        end;
+    end;
+    
+var
+    maxVentas, maxCodigo: integer;
+begin
+    maxVentas := -1;
+    maxCodigo := -1;
+    BuscarMaxVentas(a, maxVentas, maxCodigo);
+    ProductoConMasCantidadDeVentas := maxCodigo;
+end;
+
+
+var
+    arbolDeVentas: arbolVentas;
+    arbolDeTotales: arbolTotalizado;
+    arbolDeListas: arbolConLista;
+    fechaBuscada, total, codMaxTotal, codMasVentas: integer;
+
+begin
+    GenerarDatosYArboles(arbolDeVentas, arbolDeTotales, arbolDeListas);
+    writeln;
+    
+    write('Ingrese una fecha (1-31) para buscar el total de productos vendidos: ');
+    readln(fechaBuscada);
+    total := TotalVendidoEnFecha(arbolDeVentas, fechaBuscada);
+    writeln('El total de productos vendidos en la fecha ', fechaBuscada, ' fue: ', total);
+    writeln('--------------------------------------------------');
+
+    codMaxTotal := ProductoConMayorVentaTotal(arbolDeTotales);
+    writeln('El codigo de producto con mayor cantidad total de unidades vendidas es: ', codMaxTotal);
+    writeln('--------------------------------------------------');
+    
+    codMasVentas := ProductoConMasCantidadDeVentas(arbolDeListas);
+    writeln('El codigo de producto con mayor cantidad de ventas es: ', codMasVentas);
+    writeln('--------------------------------------------------');
+end.
